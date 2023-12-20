59c131a572d3bddf7aea47b16d299f8a82380ff0
Amrutavarshan L <118422092+varshan2534@users.noreply.github.com>
2023-12-20 19:01:34 +0530

    Update Login.js

--- DAY-5/src/Login.js
+++ DAY-5/src/Login.js
@@ -1,38 +1,3 @@
-// import React from 'react';
-// import './Login.css';
-// function Login(){
-//     return(
-//         <div class="loginpage">
-//             <br></br>
-//             <h4>&nbsp;&nbsp;ScheduleMeet.</h4>
-//             <br></br>
-//             <br></br>
-//         <div class="container2">
-//             <form>
-//                 <br></br>
-//                 <h1 id="head">&nbsp;&nbsp;Log in</h1>
-//                 <br></br>
-//                 &nbsp;&nbsp;&nbsp;&nbsp;<label for="uname">Username: &nbsp;</label>
-//                 <br></br>
-//                 <br></br>
-//                 &nbsp;&nbsp;&nbsp;&nbsp;<input type="text" id="uname"></input><br></br>
-//                 <br></br>
-//                 <br></br>
-//                 &nbsp;&nbsp;&nbsp;&nbsp;<label for="pwd">Password:&nbsp;&nbsp;&nbsp;</label>
-//                 <br></br>
-//                 <br></br>
-//                 &nbsp;&nbsp;&nbsp;&nbsp;<input type="password" id="pwd"></input><br></br>
-//                 <br></br>
-//                 <br></br>
-//                 <br></br>
-//                 <input type="submit" value="SUBMIT" class="but"></input>
-//             </form>
-//         </div>
-//         </div>
-//     );
-// }
-// export default Login;
-
 import React from "react";
 import './Login.css';
 import Calendar from "./Calendar";
