4223b7da298b5241db8907f219f748526f7b96bb
Amrutavarshan L <118422092+varshan2534@users.noreply.github.com>
2023-12-20 18:55:12 +0530

    Update App.js

--- DAY-2/src/App.js
+++ DAY-2/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Landing from './Landing';
 
-// import Calendar from './Calendar';
 function App() {
   return (
     <div>
-      {/* <Calendar /> */}
       <Landing />
     </div>
   );
